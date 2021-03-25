2091bcf0d71659a3f4f3b01503e64176930c8637
tnioa <hortanio@hotmail.com>
2021-03-24 21:24:32 -0300

    configs to deploy

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
-import os
-
-BASE_DIR=os.path.abspath(os.path.dirname(__file__))  
 DEBUG=True
+
+import os
+BASE_DIR=os.path.abspath(os.path.dirname(__file__))  
+
 JSON_AS_ASCII=False
-
-FLASK_ENV="development"
-FLASK_APP="myapp.app.py"
-FLASK_PORT=5000
-FLASK_DEBUG=True
-FLASK_HOST="0.0.0.0"
-
-MAIL_USERNAME="myemail@gmail.com"
-MAIL_PASSWORD="my secret password"
-
-DATABASE_URL="sqlite:///development_database.db"
