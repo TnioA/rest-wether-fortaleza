d21433036e0b67cbff3303490c14de4fb104a071
tnioa <hortanio@hotmail.com>
2021-08-23 18:15:01 -0300

    correcao main

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ class Main:
     def __init__(self):
         if __name__ == '__main__':
             host = os.getenv('FLASK_HOST')
-            port = int(os.getenv('FLASK_PORT', '5000'))
+            port = os.getenv('FLASK_PORT', default=5000)
             app.run(host=host, port=port)
 
 main = Main()
