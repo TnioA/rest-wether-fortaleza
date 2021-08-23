24a5f09baed30fb4cc39916fae292122aabeeabc
tnioa <hortanio@hotmail.com>
2021-08-23 17:50:00 -0300

    correcao main

--- Main.py
+++ Main.py
@@ -6,6 +6,6 @@ import os
 class Main:
     def __init__(self):
         if __name__ == '__main__':
-            app.run(host='0.0.0.0', port=5000)
+            app.run(host='127.0.0.1', port=5000)
 
 main = Main()
