2e0d71c1414a265e48413f1f17974f319f7ec80f
tnioa <hortanio@hotmail.com>
2021-08-23 17:39:51 -0300

    correcao main

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from App import app
 from dotenv import load_dotenv
+from os.path import join, dirname
 import os
 
 load_dotenv()
@@ -11,6 +12,6 @@ class Main:
         if __name__ == '__main__':
             host = os.getenv('FLASK_HOST', '')
             port = int(os.getenv('FLASK_PORT', 5000))
-            app.run(host=host, port=port)
+            app.run(host='0.0.0.0', port=port)
 
 main = Main()
